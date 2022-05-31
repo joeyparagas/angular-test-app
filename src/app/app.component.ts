72fea7c3c5f2752875349a6c6d363eaa7e9f14a6
joeyparagas <joey.paragas@gmail.com>
2022-05-31 14:24:12 -0700

    added Bootstrap3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'My First Angular Test File';
+  name = 'Jack';
 }
