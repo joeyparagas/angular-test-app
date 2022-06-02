d42e52a0edcebdce5e53ebf5c152f2f38bffa35a
joeyparagas <joeyparagas@fssi-ca.com>
2022-06-02 07:33:07 -0700

    mac install/update & event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+	selector: 'app-servers',
+	templateUrl: './servers.component.html',
+	styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+	allowNewServer = false;
+	serverCreationStatus = 'No server available :(';
+	serverName = 'Server Name';
 
-  ngOnInit(): void {
-  }
+	constructor() {
+		setTimeout( () => {
+			this.allowNewServer = true;
+		},2000)
+	 }
+
+	ngOnInit(): void {
+	}
+
+	onCreateServer() {
+		 this.serverCreationStatus = 'Server was created :)';
+	}
+
+	onUpdateServerName( event:any ){
+		this.serverName = (<HTMLInputElement>event.target).value;
+	}
 
 }
